40a39c5fab1f9028d44f517e30847d80f7728b8f
xunruan <xunruan@icloud.com>
2022-09-17 02:04:10 -0400

    Web Interface and Renderer
    data tells the system (web) how to render the data

--- artifacts/views.py
+++ artifacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.renderers import JSONRenderer
 
 from artifacts.models import Artifact
 from artifacts.serializers import ArtifactSerializer
@@ -9,6 +10,7 @@ from artifacts.serializers import ArtifactSerializer
 # it needs a serializer to convert the data into JSON and a queryset to get the data
 class ArtifactViewSet(viewsets.ModelViewSet):
     serializer_class = ArtifactSerializer
-
+    # ViewSet inherits from GenericAPIView, which inherits from APIView, which has renderer_classes
+    # renderer_classes = [JSONRenderer]
     def get_queryset(self):
         return Artifact.objects.all()
