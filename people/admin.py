39e42168369b95bf4b073dcd411e393778859d5c
xunruan <xunruan@icloud.com>
2022-09-17 00:24:02 -0400

    Set up view, url, admin for the model Person

--- people/admin.py
+++ people/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from people.models import Person
+
+
 # Register your models here.
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("first", "last", "title")
--- people/views.py
+++ people/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Person
+from .serializers import PersonSerializer
+
+
+@api_view(["GET"])
+def list_people(request):
+    people = Person.objects.all()  # Get all the people
+    serializer = PersonSerializer(people, many=True)  # Serialize people
+    content = {"people": serializer.data}  # Create a dictionary with the serialized people
+    return Response(content)  # Return the serialized data
+ 
